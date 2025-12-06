0f6b92ea88514b61e093dfd8238f8e154e51da69
jerrylingj <498539230@qq.com>
2025-12-06 23:56:34 +0800

    feat: review_service

--- services/sse-wiki/internal/grpc/review_service.go
+++ services/sse-wiki/internal/grpc/review_service.go
@@ -7,7 +7,7 @@ import (
 	"terminal-terrace/sse-wiki/internal/database"
 	"terminal-terrace/sse-wiki/internal/dto"
 	articleModel "terminal-terrace/sse-wiki/internal/model/article"
-	pb "terminal-terrace/sse-wiki/protobuf/proto/ssewiki"
+	pb "terminal-terrace/sse-wiki/protobuf/proto/review_service"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
